c886ee5d08fa1c32ff6d3a24633723e678dff768
João Guilherme <joaogal0425@gmail.com>
2025-12-02 10:41:34 -0300

    Bishop, Knight e Queen

---
+++ src/main/java/chess/Knight.java
@@ -0,0 +1,41 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Position;
+
+public class Knight extends ChessPiece {
+
+    public Knight(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "N"; // N de Knight (Cavalo)
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        int[][] moves = {
+            {-2, -1}, {-2, +1},
+            {-1, -2}, {-1, +2},
+            {+1, -2}, {+1, +2},
+            {+2, -1}, {+2, +1}
+        };
+
+        for (int[] m : moves) {
+            p.setValues(position.getRow() + m[0], position.getColumn() + m[1]);
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            } else if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+
+        return mat;
+    }
+}
